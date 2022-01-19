1fa676a6f90cc39c9df5ecaa1461b1fd3fcd3f33
SDog6 <90568016+SDog6@users.noreply.github.com>
2022-01-19 18:25:31 +0100

    update to docker

--- Frontend/src/Pages/Home.js
+++ Frontend/src/Pages/Home.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import jwtDecode from 'jwt-decode';
+import img from "../Images/computer-shop-various-parts-on-260nw-569212921.jpg"
 
 
 function Home() {
 
-  var tokens = localStorage.getItem("token")
-  if(tokens != null){
-    var decoded = jwtDecode(tokens);
-    console.log(decoded)
-  }
-
   return (
     <div className='home'>
-      <h1>Home</h1>
+      <h1>Wellcome</h1>
+      <br/>
+      <img src={img} alt="Img" />;
     </div>
   );
 }
